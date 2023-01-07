59a9eabed9ac5f4c0339d3e20aba85b2b2c1d46e
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-01-07 20:41:58 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,102 +1,104 @@
-import { endent, mapValues } from '@dword-design/functions'
-import puppeteer from '@dword-design/puppeteer'
+import { endent } from '@dword-design/functions'
+import tester from '@dword-design/tester'
+import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
+import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
 import { Builder, Nuxt } from 'nuxt'
 import outputFiles from 'output-files'
-import withLocalTmpDir from 'with-local-tmp-dir'
 
-let browser
-let page
+import self from './index.js'
 
-const runTest = config => () => {
-  config = { resultingId: 'logo', ...config }
-
-  return withLocalTmpDir(async () => {
-    await outputFiles({
-      'assets/logo.svg': '<svg xmlns="http://www.w3.org/2000/svg" />',
-      'pages/index.vue': config.page,
-    })
-
-    const nuxt = new Nuxt({
-      createRequire: 'native',
-      dev: false,
-      modules: [['~/../src', config.options]],
-    })
-    await new Builder(nuxt).build()
-    await nuxt.listen()
-    try {
-      await page.goto('http://localhost:3000')
-
-      const handle = await page.waitForSelector('#__layout')
-      expect(
-        await handle.evaluate(layout =>
-          layout.querySelector('svg use').getAttribute('xlink:href')
-        )
-      ).toEqual(`#${config.resultingId}`)
-      expect(
-        await page.$eval(`#${config.resultingId}`, logo => logo.tagName)
-      ).toEqual('symbol')
-    } finally {
-      await nuxt.close()
-    }
-  })
-}
-
-export default {
-  after: () => browser.close(),
-  before: async () => {
-    browser = await puppeteer.launch()
-    page = await browser.newPage()
-  },
-  ...({
+export default tester(
+  {
     jsx: {
       page: endent`
-        <script>
-        import Logo from '@/assets/logo.svg'
+      <script>
+      import Logo from '@/assets/logo.svg'
 
-        export default {
-          render() {
-            return <svg><use xlinkHref={\`#\${Logo.id}\`}></use></svg>
-          }
+      export default {
+        render() {
+          return <svg><use xlinkHref={\`#\${Logo.id}\`}></use></svg>
         }
-        </script>
+      }
+      </script>
 
-      `,
+    `,
     },
     options: {
       options: {
         symbolId: 'foo-[name]',
       },
       page: endent`
-        <script>
-        import Logo from '@/assets/logo.svg'
+      <script>
+      import Logo from '@/assets/logo.svg'
 
-        export default {
-          render() {
-            return <svg><use xlinkHref={\`#\${Logo.id}\`}></use></svg>
-          }
+      export default {
+        render() {
+          return <svg><use xlinkHref={\`#\${Logo.id}\`}></use></svg>
         }
-        </script>
+      }
+      </script>
 
-      `,
+    `,
       resultingId: 'foo-logo',
     },
     template: {
       page: endent`
-        <template>
-          <svg><use :xlink:href="'#' + Logo.id"></use></svg>
-        </template>
-        
-        <script>
-        import Logo from '@/assets/logo.svg'
+      <template>
+        <svg><use :xlink:href="'#' + Logo.id"></use></svg>
+      </template>
+      
+      <script>
+      import Logo from '@/assets/logo.svg'
 
-        export default {
-          computed: {
-            Logo: () => Logo,
-          },
-        }
-        </script>
+      export default {
+        computed: {
+          Logo: () => Logo,
+        },
+      }
+      </script>
 
-      `,
+    `,
     },
-  } |> mapValues(runTest)),
-}
+  },
+  [
+    testerPluginTmpDir(),
+    testerPluginPuppeteer(),
+    {
+      transform: config => {
+        config = { resultingId: 'logo', ...config }
+
+        return async function () {
+          await outputFiles({
+            'assets/logo.svg': '<svg xmlns="http://www.w3.org/2000/svg" />',
+            'pages/index.vue': config.page,
+          })
+
+          const nuxt = new Nuxt({
+            dev: false,
+            modules: [[self, config.options]],
+          })
+          await new Builder(nuxt).build()
+          await nuxt.listen()
+          try {
+            await this.page.goto('http://localhost:3000')
+
+            const handle = await this.page.waitForSelector('#__layout')
+            expect(
+              await handle.evaluate(layout =>
+                layout.querySelector('svg use').getAttribute('xlink:href')
+              )
+            ).toEqual(`#${config.resultingId}`)
+            expect(
+              await this.page.$eval(
+                `#${config.resultingId}`,
+                logo => logo.tagName
+              )
+            ).toEqual('symbol')
+          } finally {
+            await nuxt.close()
+          }
+        }
+      },
+    },
+  ]
+)
